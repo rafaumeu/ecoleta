dea28671e1278f38dfbe87febb2a5c7900f37e36
Rafael Dias Zendron <mmmarckos@gmail.com>
2025-01-14 11:10:20 -0300

    feat(modal, create-point): improve modal styling and add error handling
    - Update modal.css to improve styling for point-created and error-created modals
    - Add error handling for create-point form submission
    - Update create-point.html to include modal blocks for point-created and error-created
    - Update database.db to include new data
    - Update server.js to handle new data and error handling

--- src/server.js
+++ src/server.js
@@ -7,19 +7,77 @@ nunjucks.configure('src/views', {
   express: server,
   noCache: true,
 })
+server.use(express.urlencoded({ extended: true }))
 server.use(express.static('public'))
 
 server.get('/', (req, res) => {
-  res.render('index.html')
+  return res.render('index.html')
 })
 server.get('/create-point', (req, res) => {
-  res.render('create-point.html')
+  return res.render('create-point.html')
+})
+server.post('/save-point', (req, res) => {
+  db.run(`
+    CREATE TABLE IF NOT EXISTS places (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      image TEXT,
+      name TEXT,
+      address TEXT,
+      address2 TEXT,
+      state TEXT,
+      city TEXT,
+      items TEXT
+    );  
+  `)
+  const query = `
+      INSERT INTO places (
+        image,
+        name,
+        address,
+        address2,
+        state,
+        city,
+        items
+      ) VALUES (
+        ?,
+        ?,
+        ?,
+        ?,
+        ?,
+        ?,
+        ?
+      );
+    `
+  const values = [
+    req.body.image,
+    req.body.name,
+    req.body.address,
+    req.body.address2,
+    req.body.state,
+    req.body.city,
+    req.body.items,
+  ]
+
+  function afterInsertData(err) {
+    if (err) {
+      console.log(err)
+      return res.render('create-point.html', { error: true })
+    }
+    console.log('Cadastrado com sucesso!')
+    console.log(this)
+    return res.render('create-point.html', { saved: true })
+  }
+  db.run(query, values, afterInsertData)
 })
 server.get('/search', (req, res) => {
-  db.all(`SELECT * FROM places`, function (err, rows) {
+  const search = req.query.search
+  if (search == '') {
+    return res.render('search-results.html', { total: 0 })
+  }
+  db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function (err, rows) {
     if (err) return console.log(err)
     const total = rows.length
-    res.render('search-results.html', {places: rows, total})
+    return res.render('search-results.html', { places: rows, total })
   })
 })
 server.listen(3000, () => {
